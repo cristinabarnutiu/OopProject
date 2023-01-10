9c751e40c9db069296415f4001aaadac677c3d6c
cristinabarnutiu <48153999+cristinabarnutiu@users.noreply.github.com>
2023-01-10 09:04:57 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -31,11 +31,32 @@ public class Main {
         System.out.println("Gen: "+person3.gen);
 
         System.out.println(person3.gen);*/
-        Film avatar2 = new Film(false, "Avatar 2");
-        Film motanulIncaltat = new Film(true,"Motanul Incaltat");
-        Film unFilm = new Film();
-        avatar2.afiseazaPentruCopii();
-        motanulIncaltat.afiseazaPentruCopii();
-        unFilm.afiseazaPentruCopii();
+
+        Film film1 = new Film();
+        film1.setTitlu("Avatar 2");
+        film1.getTitlu();
+        film1.rating = 7.9;
+        film1.potrivitPtCopii = false;
+        //Film.unText
+
+        film1.afiseazaDacaEPotrivitPtCopii();
+        System.out.println(film1.anProductie);
+
+        Film film2 = new Film("Motanul incaltat 2", 2021, true);
+        System.out.println(film2.rating);
+        film2.afiseazaDacaEPotrivitPtCopii();
+        film2.rating = 8.0;
+
+        System.out.println(film1.afiseazaRating());
+        System.out.println(film2.afiseazaRating());
+
+        System.out.println(Film.afiseazaUnText());
+        afiseaza2();
+
     }
+
+    public static void afiseaza2(){
+        System.out.println("Afiseaza");
+    }
+
 }
